506fba6e7db18542ec9971ed7093a7a77c5ff413
pedromvgomes <pedro@smardev.com>
2020-08-18 18:33:51 +0400

    Sample completed

--- samples/nodejs/api/usersApiModule.ts
+++ samples/nodejs/api/usersApiModule.ts
@@ -1,21 +1,37 @@
 /* eslint @typescript-eslint/no-explicit-any: 0 */
 
-import * as usersData from '../data/users.json';
+import * as usersRepository from '../data/users.json';
 import { IApiModule } from './apiModule';
 import { IApiHandler, ApiHandler } from './apiHandler';
 import { ApiResult } from './apiResult';
+import { User } from '../model/model';
+import { ObjectMapConfigurationBuilder, ObjectMapper } from '@smardev/tsmapper';
 
 const apiHandlers: IApiHandler[] = new Array<ApiHandler>();
 
+const config = new ObjectMapConfigurationBuilder();
+config.map('id').from('id');
+config.map('birthDate').from('birthDate');
+config.map('email').from('email');
+config.map('userName').from('userName');
+config.map('fullName').custom((source: any) => source.first_name + ' ' + source.last_name);
+
+const users = ObjectMapper.mapArray<any, User>(config.build(), usersRepository.data, () => new User());
+
 const getAll = function (): ApiResult {
-    return ApiResult.ObjectResult(usersData.data);
+    return ApiResult.ObjectResult(users);
 };
 
 const getById = function (request: any): ApiResult {
-    const id = request.params.userId;
+    const param = request.params.userId;
+    const id: number = parseInt(param, 10);
+
+    if (isNaN(id)) {
+        return ApiResult.Error(404, 'Invalid argument');
+    }
 
     console.log('UsersApi getById Id:' + id);
-    return ApiResult.ObjectResult(usersData.data.find((i: { id: number }) => i.id.toString() === id.toString()));
+    return ApiResult.ObjectResult(users.find((user) => user.id == id.toString()));
 };
 
 apiHandlers.push(ApiHandler.get('/users', getAll));
