d05de3d26c2c6f362f3bf5c82a6628c735e8a78b
pedromvgomes <pedro@smardev.com>
2020-08-26 11:23:00 +0400

    Fixed file name case error on import

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { ObjectMapper } from './lib/objectmapper';
 export * from './lib/interfaces';
 export { MapBuilder } from './lib/mapbuilder';
 export { MapInstruction } from './lib/mapinstruction';
-export { MapInstructionBuilder } from './lib/mapInstructionbuilder';
+export { MapInstructionBuilder } from './lib/mapinstructionbuilder';
 export { MapConfiguration } from './lib/mapconfiguration';
 export { MapConfigurationBuilder } from './lib/mapconfigurationbuilder';
 export { MapProfile } from './lib/mapprofile';
